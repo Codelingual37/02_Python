f7f587dd3edad53b261dd7e021d1887f5ed94502
Codelingual37 <codelingual37@gmail.com>
2024-09-14 01:14:25 +0200

    Deleted useless entries

--- 100_Days_of_Code/Day1_Band_Name_Generator/band_name_generator.py
+++
@@ -1,4 +0,0 @@
-print("Hey, you. Can't think of a name for your band? Let me help you real quick.")
-hometown = input("What's the city you grew up in? ")
-pet = input("What's the name of your pet (or if you don't have one, what would you name it? ")
-print("So, check this out. Your band name is: " + hometown + " " + pet + "\nMake sure to remember me when you're rich.")
