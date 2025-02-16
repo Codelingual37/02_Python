ffb45337d93dc6529211c0bc7b31c20b9d28d765
Codelingual37 <codelingual37@gmail.com>
2025-02-16 21:40:53 +0100

    Simplified code

--- 100_Days_of_Code/16_OOP_Coffee_Machine/main.py
+++ 100_Days_of_Code/16_OOP_Coffee_Machine/main.py
@@ -2,28 +2,22 @@ from menu import Menu
 from coffee_maker import CoffeeMaker
 from money_machine import MoneyMachine
 
-machine_state = "on"
+machine_on = True
 
 m_machine = MoneyMachine()
 c_maker = CoffeeMaker()
 drinkmenu = Menu()
 
-while machine_state == "on":
-	coffee = input("What would you like? ").lower()
+while machine_on:
+	coffee = input(f"What would you like ({drinkmenu.get_items()})? ").lower()
 	if coffee == "off":
-		machine_state = "off"
+		machine_on = False
 		break
 	elif coffee == "report":
 		c_maker.report()
 		m_machine.report()
 		continue
 	drink = drinkmenu.find_drink(coffee)
-	if drink == None:
-		print(f"Please choose one of the following: {drinkmenu.get_items()}")
-		continue
-	if not c_maker.is_resource_sufficient(drink):
-		continue
-	if not m_machine.make_payment(drink.cost):
-		continue
-	c_maker.make_coffee(drink)
+	if c_maker.is_resource_sufficient(drink) and m_machine.make_payment(drink.cost):
+		c_maker.make_coffee(drink)
 	
